ab231ae8d18da461e399f85bf2a367139eae61b9
Jimmy Porto <ireturnerd@gmail.com>
2018-04-12 12:17:11 -0300

    Updated der and added script

---
+++ script.sql
@@ -0,0 +1,47 @@
+CREATE TABLE job (
+  idjob INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  iduserr INTEGER UNSIGNED NOT NULL,
+  name VARCHAR(50) NULL,
+  description VARCHAR(255) NULL,
+  typejob VARCHAR(20) NULL,
+  PRIMARY KEY(idjob),
+  INDEX job_FKIndex1(iduserr)
+);
+
+CREATE TABLE jobmedia (
+  idjobmedia INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  idmedia INTEGER UNSIGNED NOT NULL,
+  idjob INTEGER UNSIGNED NOT NULL,
+  idmainmedia INTEGER UNSIGNED NULL,
+  PRIMARY KEY(idjobmedia),
+  INDEX jobmedia_FKIndex1(idjob),
+  INDEX jobmedia_FKIndex2(idmedia)
+);
+
+CREATE TABLE media (
+  idmedia INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  path VARCHAR(50) NULL,
+  PRIMARY KEY(idmedia)
+);
+
+CREATE TABLE url (
+  idurl INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  idjob INTEGER UNSIGNED NOT NULL,
+  url VARCHAR(100) NULL,
+  description VARCHAR(255) NULL,
+  title VARCHAR(20) NULL,
+  PRIMARY KEY(idurl),
+  INDEX url_FKIndex1(idjob)
+);
+
+CREATE TABLE userr (
+  iduserr INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NULL,
+  bios VARCHAR(255) NULL,
+  email VARCHAR(50) NULL,
+  passwordd VARCHAR(50) NULL,
+  gituserulr VARCHAR(100) NULL,
+  PRIMARY KEY(iduserr)
+);
+
+
